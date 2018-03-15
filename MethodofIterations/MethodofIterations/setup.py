1a81f28139cb91a545a7133b8974a4bce5f3c37b
Anton Kozyrev <sonara4mus@gmail.com>
2018-03-15 20:50:41 +0200

    Update Parser
    Deleted useless staff!

--- MethodofIterations/MethodofIterations/setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "MethodOfHords",
-    version = "0.1",
-    description = "MethodOfHords",
-    executables = [Executable("MethodofIterations.py")]
-)
